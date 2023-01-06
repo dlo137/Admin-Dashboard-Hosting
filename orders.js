98fe8cb6581fd6e545e328f4eb1ee75b1721a142
D'Angelo <96030063+dlo137@users.noreply.github.com>
2023-01-06 13:37:40 -0500

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,52 @@
+const Orders = [
+    {
+        productName: 'Nightmare Factory Shirts (100)',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Make Waves T-Shirt (1)',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Malboro Long Sleeves(25)',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'HS Football Hoodies (13)',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Kobe Bootleg Shorts (22)',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: "M&M's Cafe Hoodie (10)",
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'HR Shirt(111)',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+
+]
